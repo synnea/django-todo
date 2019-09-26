4d75a8cbfb2458a1e71ff7d7b3da9eb31e68b914
Carina <carrie.poell@gmail.com>
2019-09-26 06:37:47 +0200

    Initialize Django.

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\carina\desktop\codeinstitute\01django\django_todo\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
