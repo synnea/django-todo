10473b48d125de1c4ada671ce65c4feeef4790ec
Carina <carrie.poell@gmail.com>
2019-10-01 11:43:30 +0200

    complete Hello Django

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from .models import Item
+from .forms import ItemForm
 
 # Create your views here.
 def get_todo_list(request):
@@ -8,12 +9,33 @@ def get_todo_list(request):
 
 def create_an_item(request):
     if request.method=='POST':
-        new_item = Item()
-        new_item.name = request.POST.get('name')
-        new_item.done = 'done' in request.POST
-        new_item.save()
 
-        return redirect(get_todo_list)
+        #creates a new form
+        form = ItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(get_todo_list)
 
-    return render(request, "item_form.html")
+    else:
+        form = ItemForm()
 
+    return render(request, "item_form.html", {'form': form})
+
+def edit_an_item(request, id):
+    item = get_object_or_404(Item, pk=id)
+
+    if request.method == "POST":
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect(get_todo_list)
+    else:
+        form = ItemForm(instance=item)
+    return render(request, "item_form.html", {'form': form})
+
+def toggle_status(request, id):
+    item = get_object_or_404(Item, pk=id)
+    item.done = not item.done
+    item.save()
+    return redirect(get_todo_list)
+    
