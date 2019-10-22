0fcf2a19f94dae3b3adffa277cce0e5a48e13631
Carina <carrie.poell@gmail.com>
2019-10-22 08:05:03 +0200

    get code ready for gh

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse("postgres://kzmlnwjcsrqwtv:e8a10175bde06956a2d129cf5f11f7da19987ead411e24be7766ef6ba36b2898@ec2-46-137-113-157.eu-west-1.compute.amazonaws.com:5432/d3hs1cr4j8m8h1")
 }
 
 
